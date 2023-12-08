f61245e5a320568867a17de53a121501b23adc1c
Chris Gratigny <cgratigny>
2023-12-07 22:21:15 -0800

    Update class this extends.

--- lib/apartment/active_record/active_record_monkey_patch.rb
+++ lib/apartment/active_record/active_record_monkey_patch.rb
@@ -3,10 +3,13 @@
 # https://github.com/rails/rails/commit/5f6efdab0f9acef8d04ca9a018667bfcd1cf59b5
 # Beause our schema is customer specific, we do not want to add the schema to schema.rb
 
+require 'active_record/connection_adapters/postgresql_adapter'
+require 'active_record/connection_adapters/schema_dumper'
+
 module ActiveRecord
   module ConnectionAdapters
     module PostgreSQL
-      class SchemaDumper < ConnectionAdapters::SchemaDumper
+      class SchemaDumper < ActiveRecord::ConnectionAdapters::SchemaDumper
         def schemas(stream)
         end
       end
